0f79d97cebc33d8417024a44901506a6b170519a
benjyklaut <benjkutart@gmail.com>
2026-04-07 18:10:36 +0100

    Changer quelque message d'erreur de l'anglais en francais.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,18 +4,18 @@ const JWT_SECRET = process.env.JWT_SECRET || "dev-secret-change-me";
 
 function authMiddleware(req, res, next) {
   const h = req.headers.authorization;
-  if (!h) return res.status(401).json({ error: "Missing Authorization" });
+  if (!h) return res.status(401).json({ error: "Autorisation Manquante" });
 
   const parts = h.split(" ");
   if (parts.length !== 2 || parts[0] !== "Bearer")
-    return res.status(401).json({ error: "Invalid Authorization" });
+    return res.status(401).json({ error: "Autorisation Invalide" });
 
   try {
     const payload = jwt.verify(parts[1], JWT_SECRET);
     req.user = payload;
     next();
   } catch (err) {
-    return res.status(401).json({ error: "Invalid token" });
+    return res.status(401).json({ error: "Token Invalide" });
   }
 }
 
